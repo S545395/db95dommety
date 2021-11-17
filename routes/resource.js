e89033f96897e493eb53b96acca3d518b30b85d2
S545395 <89559375+S545395@users.noreply.github.com>
2021-11-17 01:00:14 -0600

    added update/create/delete routers,views and pug

--- routes/resource.js
+++ routes/resource.js
@@ -31,4 +31,17 @@ router.get('/costumes', costume_controller.costume_list);
 /* GET costumes */ 
 router.get('/', costume_controlers.costume_view_all_Page ); 
 
+/* GET detail costume page */ 
+router.get('/detail', costume_controlers.costume_view_one_Page); 
+
+/* GET create costume page */ 
+router.get('/create', costume_controlers.costume_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', costume_controlers.costume_update_Page); 
+
+/* GET create costume page */ 
+router.get('/delete', costume_controlers.costume_delete_Page);
+
+
 module.exports = router; 
