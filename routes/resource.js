e324bc4a881d42421ed66416a712ba70f254e0bf
S545395 <89559375+S545395@users.noreply.github.com>
2021-11-28 02:12:12 -0600

    added logic for protecting update url when directly accessed

--- routes/resource.js
+++ routes/resource.js
@@ -5,11 +5,21 @@ var router = express.Router();
 // Require controller modules. 
 var api_controller = require('../controllers/api'); 
 var costume_controller = require('../controllers/costume'); 
- 
+
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /// API ROUTE /// 
  
 // GET resources base. 
-router.get('/', api_controller.api); 
+router.get('/', costume_controller.costume_list); 
  
 /// COSTUME ROUTES /// 
  
@@ -38,10 +48,11 @@ router.get('/detail', costume_controlers.costume_view_one_Page);
 router.get('/create', costume_controlers.costume_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', costume_controlers.costume_update_Page); 
+router.get('/update', secured, costume_controlers.costume_update_Page); 
 
 /* GET create costume page */ 
 router.get('/delete', costume_controlers.costume_delete_Page);
+
 
 
 module.exports = router; 
